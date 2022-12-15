bff0e501045e4a32e277b2e2e0a50f29383266c4
DeanDamianus <damianus.atisanta@ti.ukdw.ac.id>
2022-12-15 17:51:06 +0700

    Update MainActivity.kt

--- app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity.kt
+++ app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity.kt
@@ -1,6 +1,8 @@
 package id.ac.ukdw.damianus.jetpack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -10,6 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val submit1 = findViewById<Button>(R.id.tambah)
+            submit1.setOnClickListener {
 
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
     }
 }
