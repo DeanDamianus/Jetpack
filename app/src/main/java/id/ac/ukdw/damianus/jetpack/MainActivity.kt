6c7a6b441dc2a23e7fc48339754160036ae9255a
DeanDamianus <damianus.atisanta@ti.ukdw.ac.id>
2022-12-15 16:13:36 +0700

    lonely

--- app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity.kt
+++ app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity.kt
@@ -1,31 +1,40 @@
 package id.ac.ukdw.damianus.jetpack
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val firestore = FirebaseFirestore.getInstance()
 
-        val T = table("Judul", "12-12-2002", "Isi")
-        firestore.collection( "Notes").add(T).addOnSuccessListener {
-            Log.d(TAG, "penyimpanan selesai")
-        }.addOnFailureListener {
-            Log.d(TAG, "penyimpanan gagal")
+        val judul = findViewById<EditText>(R.id.judul)
+        val tanggal = findViewById<EditText>(R.id.tanggal)
+        val isi = findViewById<EditText>(R.id.isi)
+        val btn = findViewById<Button>(R.id.submit)
+
+        btn.setOnClickListener{
+            val tabel = table(judul.text.toString(), tanggal.text.toString() , isi.text.toString())
+            firestore.collection( "Notes").add(tabel).addOnSuccessListener {
+                Log.d(TAG, "Penyimpanan Berhasil")
+            }.addOnFailureListener {
+                Log.d(TAG, "Penyimpanan Gagal")
+            }
+
         }
-        var tambah = findViewById<Button>(R.id.tambah)
-         tambah.setOnClickListener {
-             val intent = Intent(this,MainActivity2::class.java)
-             startActivity(intent)
-         }
+
+
     }
 }
