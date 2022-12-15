c433ac37a3349382ab7d43698cfa0e46bf3f36d4
DeanDamianus <damianus.atisanta@ti.ukdw.ac.id>
2022-12-15 15:37:11 +0700

    test

--- app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity.kt
+++ app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity.kt
@@ -1,9 +1,13 @@
 package id.ac.ukdw.damianus.jetpack
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.widget.Button
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firestore = FirebaseFirestore.getInstance()
 
+        val N = ContactsContract.CommonDataKinds.Note("todo", "12-12-2002", "")
+        firestore.collection( "Notes").add(N).addOnSuccessListener {
+            Log.d(TAG, "penyimpanan selesai")
+        }.addOnFailureListener {
+            Log.d(TAG, "penyimpanan gagal")
+        }
         var tambah = findViewById<Button>(R.id.tambah)
          tambah.setOnClickListener {
              val intent = Intent(this,MainActivity2::class.java)
--- app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity2.kt
+++ app/src/main/java/id/ac/ukdw/damianus/jetpack/MainActivity2.kt
@@ -2,10 +2,15 @@ package id.ac.ukdw.damianus.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
     }
 }
